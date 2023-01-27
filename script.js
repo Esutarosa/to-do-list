c2b866e71bad8edc9796f3955f73ece4390bff54
Esutarosa <107695658+Esutarosa@users.noreply.github.com>
2023-01-27 22:54:47 +0200

    something

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("a");
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const taskText = taskInput.value;
+
+    const taskHTML = ``;
+});
